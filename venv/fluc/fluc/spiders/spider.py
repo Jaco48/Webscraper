b54f10a186b7bcc21782b08763b0978dde4af450
Jaco48 <kolinskij.skola@gmail.com>
2021-11-12 11:22:02 +0100

    lukas page

--- venv/fluc/fluc/spiders/spider.py
+++ venv/fluc/fluc/spiders/spider.py
@@ -6,23 +6,22 @@ import re
 
 class FlucSpider(scrapy.Spider):
     name="fluc"
-    start_urls=['https://www.fluc.at/programm/2021_Flucwoche10.html']
+    start_urls=['http://flex.at/flex_frontend/index.php?selected=programm&mode=11&id=1171']
 
     def parse(self, response):
         item = FlucItem()
-        item['title'] = response.xpath('//text()').getall()
+        item['title'] = response.xpath('//table[@class="halle"]//*/text()').getall()
 
         with open("data_file.json", "w") as filee:
             json.dump(item['title'], filee)
 
         clearstring=ClearDataString('data_file.json')
-        #print(clearstring)
+        print(clearstring)
         daylist=DayList(clearstring)
         #print(daylist)
         finished_data=JsonList(daylist)
-        print(finished_data)
+        #print(finished_data)
         WriteJson(finished_data)
-
 
 
 def ClearDataString(json_path):
@@ -34,44 +33,18 @@ def ClearDataString(json_path):
     return strdata
 
 def DayList(clearstring):
-    clear_data=[]
-    data=re.split(",Montag,|,Dienstag,|,Mittwoch,|,Donnerstag,|,Freitag,|,Samstag,|,Sonntag,",clearstring)
-    for d in data:
-        clear_data.append(d.split(','))
+    #data=re.split(",Montag,|,Dienstag,|,Mittwoch,|,Donnerstag,|,Freitag,|,Samstag,|,Sonntag,",clearstring)
+    #for d in data:
+    clear_data=clearstring.split(',')
     return clear_data
 
 def JsonList(daylist):
     finished_data={}
     finished_data["data"]=[]
-    for i in range(len(daylist)):
-        if i!=0:
-            date=daylist[i][0]+" "+weekyear
-            right=""
-            left=""
-            rightbool=False
-            for ii in range(len(daylist[i])):
-                if ii!=0:
-                    if rightbool==False:
-                        if re.search("fluc_wanne",daylist[i][ii])==None:
-                            left+=daylist[i][ii]+" "
-                        else:
-                            rightbool=True
-                    else:
-                        right+=daylist[i][ii]+" "
-            if re.search("&nbsp",left)==None:
-                finished_data['data'].append({
-                'EventName:': left,
-                'Beginn:': date,
-                'Ort:': "Fluc"
-                })
-            if right!="":
-                finished_data['data'].append({
-                'EventName:': right,
-                'Beginn:': date,
-                'Ort:': "Fluc_Wanne"
-                })
-        else:
-            weekyear=daylist[0][1]
+    finished_data['data'].append({
+    'EventName:': daylist[1],
+    'Beginn:': daylist[0][2:]
+    })
     return finished_data
 
 def WriteJson(finished_data):
